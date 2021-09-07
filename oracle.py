612f956263c77962a031393063b506ad25ee5e58
Tony Niu <tniu2001@gmail.com>
2021-09-07 21:28:34 +0800

    Fixed the mysterious bug and uploaded QRAM circuit diagram

--- oracle.py
+++ oracle.py
@@ -3,7 +3,8 @@ This file implements the phase oracle used in Task 1.
 '''
 
 
-from qiskit import QuantumCircuit, assemble, Aer, QuantumRegister, ClassicalRegister
+from qiskit import QuantumCircuit, assemble, transpile, QuantumRegister, ClassicalRegister
+from qiskit.providers.aer import AerSimulator
 from qiskit.visualization import plot_histogram
 import matplotlib.pyplot as plt
 import numpy as np
@@ -35,6 +36,7 @@ def oracle_circuit(input_vector):
                 if value_bitstring[j] == '1':
                     QRAM_circ.x(q_m[i*m + j])
         
+        QRAM_circ.barrier()
         # Then, connect register a with tau (translation from address qubits to anciliary qubits)
         QRAM_circ.x(q_tau[0])      # initializes tau_0 to |1>
         QRAM_circ.cx(q_a[0], q_tau[1])
@@ -45,17 +47,18 @@ def oracle_circuit(input_vector):
             #           the corresponding bit is 1
             for j in range(2**i):
                 QRAM_circ.ccx(q_a[i], q_tau[j], q_tau[base+j])
+
             # Step 2: use higher anciliary qubits to cancel the excitations of
             #           lower ones
             for j in range(2**i):
                 QRAM_circ.cx(q_tau[base+j], q_tau[j])
             base *= 2
-        
+        QRAM_circ.barrier()
+
         # Finally, extract the value in m register using anciliary qubits
         for k in range(2**n):
             for l in range(m):
                 QRAM_circ.ccx(q_tau[k], q_m[k*m + l], q_target[l])
-            QRAM_circ.barrier()
 
         return QRAM_circ
     
@@ -63,30 +66,73 @@ def oracle_circuit(input_vector):
     #oracle.mcx(list(range(n)), n)
     return qr, m, n
 
-def QRAM_test(address):
-    oracle, m, n = oracle_circuit([4, 9, 12, 15])
-    # Note: longer input_vector or larger contents will exceed memory limit
-    #           of the simulator (34GB memory is needed for values up to 31)
+# returns the bitstring stored in this address
+def QRAM_test(address, oracle, m, n):
+    # m is length of each bitstring stored, n is number of address qubits
     address_bitstring = list(format(address, 'b').zfill(n))
     address_bitstring.reverse()
     n_qubits = oracle.num_qubits
     prepare_circ = QuantumCircuit(n_qubits)
-    for i in range(n):
+    '''for i in range(n):
         if address_bitstring[i] == '1':
-            prepare_circ.x(i)
+            prepare_circ.x(i)'''
+    for i in range(n):
+        prepare_circ.h(i)        
     qc = prepare_circ.compose(oracle)
     c_result = ClassicalRegister(m, name='r')
     qc.add_register(c_result)
+    qc.barrier()
+    '''if address == 5:
+        qc.add_register(ClassicalRegister(1))
+        qc.measure(5, 0)
+        qc.measure(8, 1)
+        qc.draw()
+        plt.savefig('circuit_6.svg')
+        sim = AerSimulator(method='statevector')
+        tcirc = transpile(qc, sim)
+        counts = sim.run(tcirc).result().get_counts()
+        print(counts)
+        quit()'''
     qc.measure(list(range(n_qubits-m, n_qubits)), list(range(m)))
-    qobj = assemble(qc)
-    sim = Aer.get_backend("qasm_simulator")
-    counts = sim.run(qobj).result().get_counts()
-    plot_histogram(counts)
+
     #qc.draw()
-    plt.show()
+    #plt.show()
+    sim = AerSimulator(method='statevector')
+    # DO NOT use the matrix_product_state simulator!!!
+    tcirc = transpile(qc, sim)
+    counts = sim.run(tcirc).result().get_counts()
+    print(counts)
+
+
+# used to test the mysterious bit-flip bug
+# confirmed: bug is caused by the matrix_product_state simulator
+# confirmed to be bug-free
+def QRAM_debug(input_vector, results):
+    n = len(input_vector)
+    for i in range(n):
+        true_value = input_vector[i]
+        sim_value = int(results[i], 2)
+        xor_bitstring = list(format(true_value ^ sim_value, 'b'))     # 0 for correct bits, 1 for wrong bits
+        xor_bitstring.reverse()
+        flipped_bits = []
+        for j in range(len(xor_bitstring)):
+            if xor_bitstring[j] == '1':
+                flipped_bits.append(j)
+        print("Position {}, flipped bits: ".format(i), flipped_bits)
+
+
+
+
 
 if __name__ == '__main__':
-    QRAM_test(0)
-    QRAM_test(1)
-    QRAM_test(2)
-    QRAM_test(3)
+    input_vector = list([1, 5, 7, 10]) 
+    results = []
+    oracle, m, n = oracle_circuit(input_vector)
+    # oracle.draw()
+    # plt.savefig('QRAM circuit.svg')
+    QRAM_test(0, oracle, m, n)
+
+    '''for i in range(len(input_vector)):
+        results += QRAM_test(i, oracle, m, n)
+    print(results)'''
+    
