5f02a5fdc4d22950881cd86a71bf4444a9859511
Tony Niu <tniu2001@gmail.com>
2021-09-10 16:51:23 +0800

    Defined the general structure of main

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 from oracle import oracle_circuit
+from diffuser import diffuser_circuit
+from qiskit import QuantumCircuit, assemble, transpile, QuantumRegister, ClassicalRegister
+from qiskit.providers.aer import AerSimulator
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def Grovers_circuit(input_vector):
+    oracle, m, n = oracle_circuit(input_vector)
+    diffuser = diffuser_circuit(n)
+    return
+
+
+def Grovers_test(input_vector):
+    Grovers_circ = Grovers_circuit(input_vector)
+    return
+
+
+if __name__ == '__main__':
+    input_vector = [2, 3, 1, 0]
+    Grovers_test(input_vector)
